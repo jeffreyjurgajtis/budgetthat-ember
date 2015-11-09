93a901d419757f920b439d7901f2165a80f1c8a6
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2015-11-08 21:35:22 -0600

    Update authorizer to set request header access token

--- app/authorizers/custom.js
+++ app/authorizers/custom.js
@@ -2,9 +2,7 @@ import Ember from 'ember';
 import Base from 'ember-simple-auth/authorizers/base';
 
 export default Base.extend({
-  authorize: function(jqXHR, requestOptions) {
-    if (this.get('session.isAuthenticated')) {
-      jqXHR.setRequestHeader('X-ACCESS-TOKEN', this.get('session.secure.access_token'));
-    }
+  authorize: function(sessionData, setRequestHeader) {
+    setRequestHeader("X-ACCESS-TOKEN", sessionData.access_token);
   }
 });
