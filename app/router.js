dc2e322f76e73ca8bcc0e065783b1137b0d0bc73
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2015-11-09 09:06:06 -0600

    Add budgetSheet show page

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route('login');
   this.route('budgetSheets');
+  this.route('budgetSheet', { path: '/budgetSheets/:budgetSheetId' });
 });
 
 export default Router;
