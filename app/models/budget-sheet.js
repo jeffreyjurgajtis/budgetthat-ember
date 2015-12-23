a72d33b180a1d1122cf0b9770a1757534f37fb5b
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2015-12-22 23:24:32 -0600

    Add budget sheet summary functions

--- app/models/budget-sheet.js
+++ app/models/budget-sheet.js
@@ -5,5 +5,21 @@ export default DS.Model.extend({
   createdAt:  DS.attr('date', {
     defaultValue() { return new Date(); }
   }),
-  categories: DS.hasMany('category', { async: true })
+  categories: DS.hasMany('category', { async: true }),
+
+  total: function() {
+    return this.get('categories').reduce(function(previousValue, category){
+      return previousValue + category.get('budgetAmount');
+    }, 0);
+  }.property('categories.@each.budgetAmount'),
+
+  spent: function() {
+    return this.get('categories').reduce(function(previousValue, category){
+      return previousValue + category.get('entryAmountTotal');
+    }, 0);
+  }.property('categories.@each.entryAmountTotal'),
+
+  difference: function() {
+    return this.get('total') - this.get('spent');
+  }.property('total', 'spent')
 });
