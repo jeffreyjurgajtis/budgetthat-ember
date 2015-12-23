8e03bace83bae3ade3210d6c8fdd70231e1baacb
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2015-12-23 15:41:35 -0600

    Add entry-form component and addEntry action

---
+++ app/components/entry-form.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  submit(e) {
+    e.preventDefault();
+    var occurredOn  = this.occurredOn;
+    var description = this.description;
+    var categoryId  = e.target.getElementsByTagName('select')[0].value;
+    var amount      = String(this.amount).trim().replace(/\./g, '');
+
+    this.get('addEntry')(occurredOn, description, categoryId, parseInt(amount));
+    this.set('occurredOn', '');
+    this.set('description', '');
+    this.set('amount', '');
+  }
+});
--- app/controllers/budget-sheet.js
+++ app/controllers/budget-sheet.js
@@ -17,6 +17,17 @@ export default Ember.Controller.extend({
       category.save();
 
       this.model.notifyPropertyChange('categories.@each.budgetAmount'); // WHY!?
+    },
+
+    addEntry(occurredOn, description, categoryId, amount) {
+      var category = this.store.peekRecord('category', categoryId);
+
+      this.store.createRecord('entry', {
+        occurredOn: occurredOn,
+        description: description,
+        category: category,
+        amount: amount
+      });
     }
   }
 });
