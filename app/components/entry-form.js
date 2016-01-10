fb82c15ec3855884af3964df8b77ca7830293aef
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2016-01-09 23:06:54 -0600

    Add styles for entry form

--- app/components/entry-form.js
+++ app/components/entry-form.js
@@ -6,11 +6,15 @@ export default Ember.Component.extend({
     var occurredOn  = this.occurredOn;
     var description = this.description;
     var categoryId  = e.target.getElementsByTagName('select')[0].value;
-    var amount      = String(this.amount).trim().replace(/\./g, '');
+    var amount      = String(this.amount)
+      .trim()
+      .replace(/\$/g, '')
+      .replace(/\./g, '');
 
     this.get('addEntry')(occurredOn, description, categoryId, parseInt(amount));
     this.set('occurredOn', '');
     this.set('description', '');
     this.set('amount', '');
+    this.$('input.entry-occurred-on').focus();
   }
 });
