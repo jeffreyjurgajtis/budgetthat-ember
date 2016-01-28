ee6e410ec8cc723bfdac01b964c268a2d72271ff
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2016-01-28 11:44:21 -0600

    Add failure flash for failed login

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -2,20 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   session: Ember.inject.service(),
+  flashMessages: Ember.inject.service(),
 
   classNames: ['login'],
 
   actions: {
     authenticate: function() {
-      var credentials = this.getProperties('email', 'password');
+      const flashMessages = this.get('flashMessages');
+      let credentials = this.getProperties('email', 'password');
 
       this.get('session')
         .authenticate('authenticator:custom', credentials)
         .then(function() {
       }, function() {
-        console.log('authentication failed!');
+        flashMessages.failure('Invalid email/password.');
       });
-
     }
   }
 });
