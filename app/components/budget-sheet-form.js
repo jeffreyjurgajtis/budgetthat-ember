0d207292c1f0ea5274557899ea471e7cc801a9d8
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2016-02-04 21:52:14 -0500

    Add error handling to budget sheet form component

--- app/components/budget-sheet-form.js
+++ app/components/budget-sheet-form.js
@@ -3,9 +3,21 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['budget-sheet-card', 'budget-sheet-card--inverted'],
 
+  nameValid: true,
+  nameInvalid: Ember.computed.not('nameValid'),
+
+  focusIn() {
+    this.set('nameValid', true);
+  },
+
   submit(e) {
     e.preventDefault();
-    this.get('addBudgetSheet')(this.name);
-    this.set('name', '');
+
+    if (Ember.isPresent(this.name)) {
+      this.get('addBudgetSheet')(this.name);
+      this.set('name', '');
+    } else {
+      this.set('nameValid', false);
+    }
   }
 });
