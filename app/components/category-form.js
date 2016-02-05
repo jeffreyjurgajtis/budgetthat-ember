c94985e92ae9ff2085c728fe1dcb4b438a461b7e
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2016-02-04 21:22:41 -0500

    Add errors to category form component

--- app/components/category-form.js
+++ app/components/category-form.js
@@ -1,17 +1,49 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  submit(e) {
-    e.preventDefault();
-    let budgetAmount = String(this.budgetAmount)
+  nameValid: true,
+  amountValid: true,
+  nameInvalid: Ember.computed.not('nameValid'),
+  amountInvalid: Ember.computed.not('amountValid'),
+  valid: Ember.computed.and('nameValid', 'amountValid'),
+
+  budgetAmountInt: Ember.computed('budgetAmount', function() {
+    let value = String(this.budgetAmount)
       .trim()
       .replace(/\$/g, '')
       .replace(/\./g, '');
 
-    this.attrs.categoryAdded(this.name, parseInt(budgetAmount));
+    return parseInt(value);
+  }),
 
-    this.set('name', '');
-    this.set('budgetAmount', '');
-    this.$('input.category-name').focus();
+  submit(e) {
+    e.preventDefault();
+    let budgetAmount = this.get('budgetAmountInt');
+    let name = this.name;
+
+    if (Ember.isBlank(name)) {
+      this.set('nameValid', false);
+    }
+
+    if (!Number.isInteger(budgetAmount)) {
+      this.set('amountValid', false);
+    }
+
+    if (this.get('valid')) {
+      this.attrs.categoryAdded(this.name, budgetAmount);
+      this.set('name', '');
+      this.set('budgetAmount', '');
+      this.$('input.category-name').focus();
+    }
+  },
+
+  actions: {
+    resetNameError() {
+      this.set('nameValid', true);
+    },
+
+    resetAmountError() {
+      this.set('amountValid', true);
+    }
   }
 });
