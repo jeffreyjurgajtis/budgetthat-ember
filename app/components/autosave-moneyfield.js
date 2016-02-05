5da3ec3c0a7acd5649fab135f986632c68e37c7d
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2016-02-04 20:17:40 -0500

    Add moneyfield error display

--- app/components/autosave-moneyfield.js
+++ app/components/autosave-moneyfield.js
@@ -2,18 +2,29 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['field-wrapper'],
+  inputInvalid: false,
+
+  focusIn() {
+    this.set('inputInvalid', false);
+  },
 
   focusOut(e) {
     const attributeName = this.get('attributeName');
     const recordId = this.get('recordId');
-    const value = String(e.target.value)
+    let value = String(e.target.value)
       .trim()
       .replace(/\$/g, '')
       .replace(/\./g, '');
-    this.attrs.recordChanged(recordId, attributeName, parseInt(value));
+    value = parseInt(value);
+
+    if (Number.isInteger(value)) {
+      this.attrs.recordChanged(recordId, attributeName, value);
+    } else {
+      this.set('inputInvalid', true);
+    }
   },
 
-  Amount: Ember.computed('attributeValue', function() {
+  amount: Ember.computed('attributeValue', function() {
     let number = this.get('attributeValue');
     return (number * 0.01).toFixed(2);
   })
