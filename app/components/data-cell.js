2c8928bc0c7f13da83c51a88d1b060f01a403906
Jeffrey Jurgajtis <jeffreyjurgajtis@gmail.com>
2016-04-24 15:18:53 -0500

    Highlight positive data cells as green

--- app/components/data-cell.js
+++ app/components/data-cell.js
@@ -5,11 +5,18 @@ export default Ember.Component.extend({
 
   classNameBindings: [
     'isNegative:data-cell--negative',
+    'isPositive:data-cell--positive',
     'alignRight:data-cell--align-right',
     'isBold:data-cell--bold'
   ],
 
-  isNegative: Ember.computed(function() {
-    return this.amount < 0;
-  }).property('amount')
+  isNegative: Ember.computed('amount', function() {
+    return this.get('amount') < 0;
+  }),
+
+  amountPositive: Ember.computed('amount', function() {
+    return this.get('amount') > 0;
+  }),
+
+  isPositive: Ember.computed.and('highlightPositive', 'amountPositive')
 });
